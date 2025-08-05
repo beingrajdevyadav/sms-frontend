36b0ec60e41e1b7aae702d235d003a2fc5413c7a
Rajdev Yadav <myself.rajdev2024@gmail.com>
2025-08-05 19:04:19 +0530

    navbar styled.

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,19 +1,22 @@
 import React from 'react'
 import { NavLink } from 'react-router-dom'
+import '../css/navbar.css'
+
 
 const Navbar = () => {
     return (
-        <div>
+        <div className='navbar'>
             <div className='top-nav'>
                 <div className="logo">
                     <h1>SMS</h1>
                 </div>
 
+
                 <ul>
-                    <li><NavLink to={"/"}>Home</NavLink></li>
-                    <li><NavLink to={'/create'}>Create</NavLink></li>
-                    <li><NavLink to={'/edit'}>Edit</NavLink></li>
-                    <li><NavLink to={'/delete'}>Delete</NavLink></li>
+                    <li><NavLink to={"/"}><i className="fa-solid fa-house-chimney-user"></i> Home</NavLink></li>
+                    <li><NavLink to={'/create'}><i className="fa-solid fa-plus"></i> Create</NavLink></li>
+                    <li><NavLink to={'/edit'}><i className="fa-solid fa-pen-to-square"></i> Edit</NavLink></li>
+                    <li><NavLink to={'/delete'}><i className="fa-solid fa-trash"></i> Delete</NavLink></li>
                 </ul>
             </div>
 
