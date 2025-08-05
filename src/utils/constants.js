8621b8965f5d74403623b1e681e80a81ed112c3c
Rajdev Yadav <myself.rajdev2024@gmail.com>
2025-08-05 18:16:08 +0530

    constant.js file added.

---
+++ src/utils/constants.js
@@ -0,0 +1,5 @@
+export const BASE_API_URL = "http://localhost:5000/students";
+export const CODER_NAME = "Rajdev Yadav";
+export const PROJECT_NAME = "SMS";
+export const LINKEDIN_URL = "";
+export const GITHUB_URL = "";
