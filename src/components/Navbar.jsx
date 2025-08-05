93c0cb3e652c70f89dea219027374b12a1dbdc64
Rajdev Yadav <myself.rajdev2024@gmail.com>
2025-08-05 17:48:58 +0530

    navbar created.

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,11 +1,27 @@
 import React from 'react'
+import { NavLink } from 'react-router-dom'
 
 const Navbar = () => {
   return (
     <div>
-        <div className="logo">
+        <div className='top-nav'>
+  <div className="logo">
             <h1>SMS</h1>
         </div>
+
+        <ul>
+            <li><NavLink to={"/"}>Home</NavLink></li>
+            <li><NavLink to={'/create'}>Create</NavLink></li>
+            <li><NavLink to={'/edit'}>Edit</NavLink></li>
+            <li><NavLink to={'/delete'}>Delete</NavLink></li>
+            
+        </ul>
+        </div>
+      
+
+      <div className="bottom-nav">
+        <p>SMS Powered By <NavLink to={"/about"}>Rajdev Yadav</NavLink> </p>
+      </div>
     </div>
   )
 }
