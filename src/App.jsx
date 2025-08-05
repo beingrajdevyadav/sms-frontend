f0a2b17edfe0d91fa6fa3740d2e5ce349a3430fc
Rajdev Yadav <myself.rajdev2024@gmail.com>
2025-08-05 21:33:42 +0530

    form styles seperated in another css file.

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
 import React from 'react'
-import './App.css'
 import Navbar from './components/Navbar'
 import { Route, Routes } from 'react-router-dom'
 import Home from './pages/Home'
@@ -7,6 +6,8 @@ import Create from './pages/Create'
 import Edit from './pages/Edit'
 import Delete from './pages/Delete'
 import About from './pages/About'
+import './App.css';
+import './css/form.css'
 
 
 const App = () => {
