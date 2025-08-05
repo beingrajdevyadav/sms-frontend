160a2d528511547076d3ec9ea195c6eff05657d1
Rajdev Yadav <myself.rajdev2024@gmail.com>
2025-08-05 16:36:31 +0530

    students record table added.

--- src/components/StudentTable.jsx
+++ src/components/StudentTable.jsx
@@ -1,9 +1,45 @@
 import React from 'react'
 
-const StudentTable = () => {
+const StudentTable = ({ students, page, totalPages, setPage }) => {
   return (
-    <div>StudentTable</div>
+    <div>
+      <h2>Students</h2>
+      <hr />
+
+      <table border={1} cellPadding={10}>
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Age</th>
+            <th>City</th>
+            <th>Course</th>
+            <th>Admission Date</th>
+          </tr>
+        </thead>
+        <tbody>
+          {
+            students.length?(
+              students.map((student)=>(
+                <tr key={student._id}>
+                  <td>{student.name}</td>
+                  <td>{student.age}</td>
+                  <td>{student.city}</td>
+                  <td>{student.course}</td>
+                  <td>{new Date(student.admissionDate).toLocaleDateString()}</td>
+                </tr>
+              ))
+            ): (
+              <tr>
+                <td style={{textAlign:"center"}} colSpan={5}>No students found</td>
+              </tr>
+            )
+          }
+        </tbody>
+      </table>
+
+      
+    </div>
   )
 }
- 
+
 export default StudentTable
